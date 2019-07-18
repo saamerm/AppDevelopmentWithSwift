7b77229cf1886529514fd99b54ebcd6b0abeea34
Fadoua Amrou <faitamrou.90@gmail.com>
2019-07-18 10:10:34 +0530

    Created HelloWorld in Terminal using Swift Repl

---
+++ Unit 1/Lesson 1.1/HelloWorld.swift
@@ -0,0 +1,4 @@
+print("Hello, World!")
+// To run the command, open the directory of this file in terminal and then run "swift HelloWorld.swift"
+// or you can just say "swift" and that will open the Swift REPL(Read, Eval, Print Loop). and then type in 
+// print("Hello, World!") and press enter. Then to exit, just say ":quit" 
