9b497556fb960d18420d18782a8e091f3e4568f0
Saamer Mansoor <i@saamer.me>
2019-10-27 23:41:43 -0400

    Lesson 1.5 completed from Unit 1

---
+++ Unit 1/Lesson 1.5/GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Saamer Mansoooor on 7/22/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let _ = 4
+        _ = 5
+        var _ = 3
+    }
+
+
+}
+
